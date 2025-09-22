9ff09223d2e737341833fa514db1be7911db73fd
Elena Marcu <elenamarcu070@gmail.com>
2025-09-22 12:27:25 +0300

    Fix wsgi import path

--- rezervari_spalatorie/asgi.py
+++ rezervari_spalatorie/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rezervari_sali.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rezervari_spalatorie.settings')
 
 application = get_asgi_application()
--- rezervari_spalatorie/wsgi.py
+++ rezervari_spalatorie/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rezervari_sali.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rezervari_spalatorie.settings')
 
 application = get_wsgi_application()
