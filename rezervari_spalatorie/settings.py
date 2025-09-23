68250ec3e48e99c11cf01d303148bf842dd84bfd
Elena Marcu <elenamarcu070@gmail.com>
2025-09-23 10:20:12 +0300

    Fix settings.py

--- rezervari_spalatorie/settings.py
+++ rezervari_spalatorie/settings.py
@@ -22,6 +22,8 @@ else:
 
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "")
 CSRF_TRUSTED_ORIGINS = [o.strip() for o in CSRF_TRUSTED_ORIGINS.split(",") if o.strip()]
+
+
 # --------------------
 # APLICAȚII
 # --------------------
