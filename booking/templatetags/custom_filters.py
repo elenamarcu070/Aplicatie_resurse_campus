461d1db4cc203e5b9c1cc9f15cb45e073c356e5c
Elena Marcu <elenamarcu070@gmail.com>
2025-10-05 13:17:37 +0300

    telefon admini

--- booking/templatetags/custom_filters.py
+++ booking/templatetags/custom_filters.py
@@ -16,3 +16,14 @@ def dict_get(dict_obj, key):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+from booking.models import AdminCamin
+
+
+@register.filter
+def admin_telefon(email):
+    try:
+        admin = AdminCamin.objects.filter(email=email).first()
+        return admin.telefon if admin and admin.telefon else "-"
+    except Exception:
+        return "-"
