9bc0c1215985cf01f19e86682d4ec469e1be7342
Elena Marcu <elenamarcu070@gmail.com>
2025-10-05 20:39:26 +0300

    sms pe twilio

--- booking/utils.py
+++ booking/utils.py
@@ -1,11 +1,9 @@
-
 # booking/utils.py
 from twilio.rest import Client
 from django.conf import settings
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 def trimite_sms(numar, mesaj):
     """Trimite SMS prin Twilio cu expeditor alfanumeric WASHTUIASI."""
@@ -21,13 +19,9 @@ def trimite_sms(numar, mesaj):
         client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
         msg = client.messages.create(
             to=numar,
-            from_=settings.TWILIO_PHONE_NUMBER
-            #from_="WASHTUIASI",  # Expeditor personalizat
+            from_="WASHTUIASI",  # Expeditor personalizat
             body=mesaj,
         )
         logger.info(f"✅ Twilio: SID={msg.sid}, STATUS={msg.status}")
     except Exception as e:
         logger.error(f"💥 Eroare Twilio SMS: {e}")
-
-
-
