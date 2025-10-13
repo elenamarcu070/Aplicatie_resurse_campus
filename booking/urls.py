73d8a532ecd75fad0b95b9ee0e72cac598ba35f9
Elena Marcu <elenamarcu070@gmail.com>
2025-10-13 11:21:13 +0300

    Update urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     # Dashboard Admin Cămin
     path('dashboard/admin_camin/', views.dashboard_admin_camin, name='dashboard_admin_camin'),
 
-    path('testeaza_whatsapp/', views.testeaza_whatsapp, name='testeaza_whatsapp'),
+   
 
     # Admin cămin - Detalii cămin
     path('dashboard/admin_camin/camine/<int:camin_id>/', views.detalii_camin_admin, name='detalii_camin_admin'),
