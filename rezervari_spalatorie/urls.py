3ea427ea8cb9eda9ea67c408fce25264a1a18c83
Elena Marcu <elenamarcu070@gmail.com>
2025-10-29 15:42:36 +0200

    salvare

--- rezervari_spalatorie/urls.py
+++ rezervari_spalatorie/urls.py
@@ -1,5 +1,7 @@
 # rezervari_sali/urls.py
 
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 from django.urls import path, include
 from booking import views as booking_views
@@ -14,4 +16,14 @@ urlpatterns = [
     path("accounts/signup/", RedirectView.as_view(pattern_name="account_login", permanent=False)),  # blocăm signup
     path('accounts/', include('allauth.urls')),
     path('logout/', booking_views.custom_logout, name='custom_logout'),
+
+    
+    path(
+        "firebase-messaging-sw.js",
+        TemplateView.as_view(
+            template_name="firebase-messaging-sw.js",
+            content_type="application/javascript"
+        ),
+        name="firebase_sw",
+    ),
 ]
